97feeb18e81426490e88f82de4bdf2f7967cfc88
ParkerGates <PARKER.GATES1052@STU.MTEC.EDU>
2022-05-23 14:48:58 -0600

    starting to changing context type to reducer

--- src/classes/FlashcardGame.ts
+++ src/classes/FlashcardGame.ts
@@ -1,10 +1,11 @@
+import { allKeys } from '../context/data';
 import shuffle from "../utilities/shuffleArray";
 
 export default class FlashcardGame {
     private keySet:"treble" | "bass" | "upperTreble" | "lowest" | "highest";
     private keySetIndexRange: number[];
     private keySetNotes: string[];
-    private allNotes = ["a0","b0","c1","d1","e1","f1","g1","a1","b1","c2","d2","e2","f2","g2","a2","b2","c3","d3","e3","f3", "g3","a3","b3","c4","d4","e4","f4","g4","a4","b4","c5","d5","e5","f5","g5","a5","b5","c6","d6","e6","f6","g6","a6","b6","c7","d7","e7","f7","g7","a7","b7","c8"]
+    private allNotes = allKeys
 
 
     constructor(keySet: "treble" | "bass" | "upperTreble" | "lowest" | "highest") {
@@ -69,5 +70,9 @@ export default class FlashcardGame {
             case "lowest":
                 return [0,12];  //note range index
         }
+    }
+
+    public initProbabilityPool() {
+
     }
 }
--- src/context/context.tsx
+++ src/context/context.tsx
@@ -1,16 +1,43 @@
-import React from 'react';
-import { useState, useContext } from 'react';
+import React, { useReducer } from 'react';
+import { useContext } from 'react';
+import noteData from './data'
 
+interface Props {
+    children: JSX.Element;
+}
+
+//Context
 const ScoreContext = React.createContext("");
+
+//Context for Class Components
+const ScoreConsumer = ScoreContext.Consumer;
+export {ScoreContext, ScoreConsumer}
 
 export function useScore() {
     return useContext(ScoreContext);
 }
 
-export default function AppContext() {
-    const [score, setScore] = useState("yee");
+const initialState = noteData;
+
+const reducer = (state:any , action:any) => {
+    switch(action.type) {
+        case "test1":
+            return state;
+        case "test2":
+            return state;
+        case "reset":
+            return initialState;
+        default:
+            return state;
+    }
+}
+
+export default function AppContext(props: Props) {
+    const [noteScore, dispatch] = useReducer(reducer, initialState);
 
     return(
-        ""
+        <ScoreContext.Provider value={"a"}>
+            {props.children}
+        </ScoreContext.Provider>
     );
 }
