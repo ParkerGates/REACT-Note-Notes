b7eef42191a5c1ac24d7571486ea21b973241ffe
ParkerGates <PARKER.GATES1052@STU.MTEC.EDU>
2022-05-23 20:48:03 -0600

    start button and count down added

--- src/views/flashcards.tsx
+++ src/views/flashcards.tsx
@@ -1,22 +1,51 @@
-import { useContext} from 'react';
+import { useState, useContext} from 'react';
 import { useScore, ScoreContext } from '../context/context';
 import FlashcardGame from "../classes/FlashcardGame";
 
 export default function Flashcards() {
-    let test = new FlashcardGame("treble");
-    const data = useContext(ScoreContext)
+    const [gameState, setGameState] = useState<"no-game"|"pre-game"|"game"|"post-game">("no-game");
+    const [countdown, setCountDown] = useState<number>(3);
+    
+    //let test = new FlashcardGame("treble");
+    //const data = useContext(ScoreContext)
     //console.log(data.scoreState.noteData);
     
-    test.countProbabilityPool(data.scoreState.noteData);
+    const startGameCountdown = () => {
+        setGameState("pre-game");
+        const countDownInterval = setInterval(() => {
 
-    const TestThing = () => {
-        //data.scoreDispatch({type: "test1"});
+            setCountDown((prevState) => {
+                if (prevState <= 1) {
+                    clearInterval(countDownInterval);
+                    setGameState("game");
+                    return prevState;
+                }
+                return prevState - 1
+            });
+        }, 1000);
     }
+
+
 
     return(
         <div>
             <h4>Flashcards</h4>
-            <button onClick={TestThing}>Test</button>
+            { "no-game" === gameState &&
+                <div>
+                    <button onClick={startGameCountdown}>Start</button>
+                </div>
+            }
+            { "pre-game" === gameState &&
+                <div>
+                    <h3>{countdown}</h3>
+                </div>
+            }
+            { "game" === gameState && 
+                "lez go"
+            }
+            { "post-game" === gameState &&
+                "sigh hawaii"
+            }
         </div>
     );
 }
