42b34506f86cd5aeb05feb422fe86a61cd5556ea
Parker Gates <parkergates14@gmail.com>
2022-06-14 05:28:07 -0600

    review, about and profile view added

--- src/App.tsx
+++ src/App.tsx
@@ -3,6 +3,9 @@ import { BrowserRouter, Routes, Route, Link } from 'react-router-dom';
 import Home from './views/home';
 import Setup from './views/setup';
 import Flashcards from './views/flashcards';
+import Review from './views/review';
+import About from './views/about';
+import Profile from './views/profile';
 import './App.css';
 
 function App() {
@@ -13,13 +16,19 @@ function App() {
           <Link to="/">Home</Link>
           <Link to="setup">Setup</Link>
           <Link to="flashcards">Flashcards</Link>
+          <Link to="review">Review</Link>
+          <Link to="about">About</Link>
+          <Link to="profile">Profile</Link>
         </div>
       </nav>
 
       <Routes>
-        <Route path="/" element={<Home />}></Route>
-        <Route path="/setup" element={<Setup />}></Route>
-        <Route path="/flashcards" element={<Flashcards />}></Route>
+        <Route path="/" element={<Home />} />
+        <Route path="/setup" element={<Setup />} />
+        <Route path="/flashcards" element={<Flashcards />} />
+        <Route path="/review" element={<Review />} />
+        <Route path="/about" element={<About />} />
+        <Route path="/profile" element={<Profile />} />
       </Routes>
     </BrowserRouter>
   );
