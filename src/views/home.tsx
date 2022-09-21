34fd9336bcf00908f603aa7ee5a7a1acb8f780ff
Parker Gates <parkergates14@gmail.com>
2022-09-20 19:36:15 -0600

    home page blocking 1 and bot bar start
    bottom bar images added and home page content blocking started

--- src/views/home.tsx
+++ src/views/home.tsx
@@ -1,13 +1,39 @@
 import DotsLong from "../svgs/DotsLong.svg";
-import '../App.css';
+import BotHome from "../svgs/BotHome2.svg";
+import Logo from "../components/Logo/Logo";
 import "./css/home.css";
+import '../App.css';
+
 
 export default function Home() {
 
     return(
-        <>
-            <div className="displayFlexCenter">
-                <h1>Welcome Home</h1>
+        <>  
+            <div className="ResponsiveFlexTest">
+                <div className="LogoHome">
+                    <Logo size="Large" width="100%"/>
+                </div>
+
+                <div className="section1Home">
+                    <h1 className="s1TitleHome">Learn Music<br />Notes The Fast Way</h1>
+
+                    <div className="s1ContentHome">
+                        Our guided learning flashcard system simplifies smooths out the learning the curve making learn sheet
+                        music as easy as 1 2 3
+                    </div>
+
+                    <div className="s1CallAndBtn">
+                        <div className="s1CallToActionHome">Lets Hop To It!</div>
+
+                        <div className="s1BtnContainerHome">
+                            <button className="btnPlain guestBtnHome">Guest</button>
+                            <div>
+                                <button className="btnPlain loginBtnHome">Login</button>
+                                <button className="btnGradiant signupBtnHome">Sign Up</button>
+                            </div>
+                        </div>
+                    </div>
+                </div>
             </div>
             {/* <div><i className="fa fa-home" aria-hidden="true"> Home</i></div>
             <div><i className="fa fa-question-circle-o" aria-hidden="true"></i> About</div>
@@ -18,6 +44,7 @@ export default function Home() {
 
             <div className="homeBg">
                 <img className="dotsHome" src={DotsLong} alt="dots" />
+                <img className="botHome" src={BotHome} alt="gradiant slope" />
             </div>
         </>
     );
--- src/views/profile.tsx
+++ src/views/profile.tsx
@@ -1,15 +1,20 @@
 import React from 'react';
+import BoxSection from '../components/CssComp/BoxSection/BoxSection';
 import DotsLong from "../svgs/DotsLong.svg";
+import BotProfile from "../svgs/BotProfile.svg";
 import "./css/profile.css";
 
 export default function Profile() {
 
     return (
-        <div>
-            <h1>Profile</h1>
+        <div className="containerProfile">
+            <div className="contentProfile">
+                testing
+            </div>
 
             <div className="profileBg">
                 <img className="dotsProfile" src={DotsLong} alt="dots" />
+                <img className="botProfile" src={BotProfile} alt="gradiant slope" />
             </div>
         </div>
     );
