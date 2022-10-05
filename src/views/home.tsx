9d349d7ae19582860fb83c8fd799f9db40480eb9
Parker Gates <parkergates14@gmail.com>
2022-10-04 21:30:24 -0600

    Home Page Visual Complete

--- src/views/home.tsx
+++ src/views/home.tsx
@@ -8,7 +8,7 @@ import '../App.css';
 export default function Home() {
 
     return(
-        <>  
+        <div className="HomeContainer">  
             <div className="ResponsiveFlexTest">
                 <div className="LogoHome">
                     <Logo size="Large" width="100%"/>
@@ -35,17 +35,10 @@ export default function Home() {
                     </div>
                 </div>
             </div>
-            {/* <div><i className="fa fa-home" aria-hidden="true"> Home</i></div>
-            <div><i className="fa fa-question-circle-o" aria-hidden="true"></i> About</div>
-            <div><i className="fa fa-th-large" aria-hidden="true"></i> Flashcards</div>
-            <div><i className="fa fa-graduation-cap" aria-hidden="true"></i> Review</div>
-            <div><i className="fa fa-eye" aria-hidden="true"></i> Show Case</div>
-            <div><i className="fa fa-user-circle" aria-hidden="true"></i> Profile</div> */}
-
-            <div className="homeBg">
-                <img className="dotsHome" src={DotsLong} alt="dots" />
-                <img className="botHome" src={BotHome} alt="gradiant slope" />
-            </div>
-        </>
+            
+            <img className="dotsHome" src={DotsLong} alt="dots" />
+            <img className="botHome" src={BotHome} alt="gradiant slope" />
+            
+        </div>
     );
 }
