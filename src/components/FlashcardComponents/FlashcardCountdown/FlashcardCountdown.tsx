c9b478d3d9fc0a56192a130fe6534bf8b2efe4ca
Parker Gates <parkergates14@gmail.com>
2023-07-09 21:40:58 -0600

    flaschard notes have images for testing

--- src/components/FlashcardComponents/FlashcardCountdown/FlashcardCountdown.tsx
+++ src/components/FlashcardComponents/FlashcardCountdown/FlashcardCountdown.tsx
@@ -6,7 +6,7 @@ export default function FlashcardCountdown({countDownNumber}: Props) {
 
     return (
         <>
-            <h1 style={{fontSize:"80px"}}>{countDownNumber}</h1>
+            <h1 style={{fontSize:"80px", marginBottom:"8vh"}}>{countDownNumber}</h1>
         </>
     );
 }
---
+++ src/svgs/notes.ts
@@ -0,0 +1,39 @@
+import treblec4 from "../svgs/Notes/Treble/c4.svg"; import trebled4 from "../svgs/Notes/Treble/d4.svg";
+import treblee4 from "../svgs/Notes/Treble/e4.svg"; import treblef4 from "../svgs/Notes/Treble/f4.svg";
+import trebleg4 from "../svgs/Notes/Treble/g4.svg"; import treblea4 from "../svgs/Notes/Treble/a4.svg";
+import trebleb4 from "../svgs/Notes/Treble/b4.svg"; import treblec5 from "../svgs/Notes/Treble/c5.svg";
+import trebled5 from "../svgs/Notes/Treble/d5.svg"; import treblee5 from "../svgs/Notes/Treble/e5.svg";
+import treblef5 from "../svgs/Notes/Treble/f5.svg";
+import bassg2 from "../svgs/Notes/Bass/g2.svg"; import bassa2 from "../svgs/Notes/Bass/a2.svg";
+import bassb2 from "../svgs/Notes/Bass/b2.svg"; import bassc3 from "../svgs/Notes/Bass/c3.svg";
+import bassd3 from "../svgs/Notes/Bass/d3.svg"; import basse3 from "../svgs/Notes/Bass/e3.svg";
+import bassf3 from "../svgs/Notes/Bass/f3.svg"; import bassg3 from "../svgs/Notes/Bass/g3.svg";
+import bassa3 from "../svgs/Notes/Bass/a3.svg"; import bassb3 from "../svgs/Notes/Bass/b3.svg";
+import bassc4 from "../svgs/Notes/Bass/c4.svg";
+
+const noteImgIndex = {
+    treblec4,
+    trebled4,
+    treblee4,
+    treblef4,
+    trebleg4,
+    treblea4,
+    trebleb4,
+    treblec5,
+    trebled5,
+    treblee5,
+    treblef5,
+    bassg2,
+    bassa2,
+    bassb2,
+    bassc3,
+    bassd3,
+    basse3,
+    bassf3,
+    bassg3,
+    bassa3,
+    bassb3,
+    bassc4,
+}
+
+export default noteImgIndex;
