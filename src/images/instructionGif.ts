f13ed3f8474e5114b650d8a39d5e61ec7908535c
Parker Gates <parkergates14@gmail.com>
2023-07-12 19:44:03 -0600

    gif index added, gifs added to page

---
+++ src/images/instructionGif.ts
@@ -0,0 +1,33 @@
+import DesktopGuestSignin from "../images/Desktop/DGuestSignin.gif";
+import DesktopFlashcards from "../images/Desktop/DFlashcards.gif";
+import DesktopKeyset from "../images/Desktop/DKeyset.gif";
+import DesktopTest from "../images/Desktop/DTest.gif";
+import DesktopStart from "../images/Desktop/DStart.gif";
+import DesktopGuess from "../images/Desktop/DGuess.gif";
+import DesktopEndGame from "../images/Desktop/DEndGame.gif";
+import MobileGuestSignin from "../images/Mobile/MGuestSignin.gif";
+import MobileFlashcards from "../images/Mobile/MFlashcards.gif";
+import MobileKeyset from "../images/Mobile/MKeyset.gif";
+import MobileTest from "../images/Mobile/MTest.gif";
+import MobileStart from "../images/Mobile/MStart.gif";
+import MobileGuess from "../images/Mobile/MGuess.gif";
+import MobileEndGame from "../images/Mobile/MEndGame.gif";
+
+const InstrGif = {
+    DesktopGuestSignin,
+    DesktopFlashcards,
+    DesktopKeyset,
+    DesktopTest,
+    DesktopStart,
+    DesktopGuess,
+    DesktopEndGame,
+    MobileGuestSignin,
+    MobileFlashcards,
+    MobileKeyset,
+    MobileTest,
+    MobileStart,
+    MobileGuess,
+    MobileEndGame,
+}
+
+export default InstrGif;
--- src/views/showcase.tsx
+++ src/views/showcase.tsx
@@ -1,8 +1,9 @@
 import BoxSection from "../components/CssComp/BoxSection/BoxSection";
 import GradientScrollElement from "../components/GradientScrollElement/GradientScrollElement";
+import InstrGif from "../images/instructionGif";
 import DotsLong from "../svgs/Background/DotsLong.svg";
 import "./css/showcase.css";
-import 'animate.css';
+import "animate.css";
 
 export default function Showcase() {
 
@@ -14,27 +15,40 @@ export default function Showcase() {
 
                     <BoxSection title={"1: Log in"}>
                             <h3>Select Guest Account</h3>
-
+                            <img className="desktopGif" src={InstrGif.DesktopGuestSignin} alt="How to sign in gif" />
+                            <img className="mobileGif" src={InstrGif.MobileGuestSignin} alt="How to sign in gif" />
                             <h3>OR Select Guest Account</h3>
                     </BoxSection>
                     <br />
 
                     <BoxSection title={"2: Create Game"}>
                             <h3>Go To Flashcard Page</h3>
+                            <img className="desktopGif" src={InstrGif.DesktopFlashcards} alt="Go to flashcard page gif" />
+                            <img className="mobileGif" src={InstrGif.MobileFlashcards} alt="Go to flashcard page gif" />
 
                             <h3>Select Keyset</h3>
+                            <img className="desktopGif" src={InstrGif.DesktopKeyset} alt="Select keyset gif" />
+                            <img className="mobileGif" src={InstrGif.MobileKeyset} alt="select keyset gif" />
 
                             <h3>Select Test</h3>
+                            <img className="desktopGif" src={InstrGif.DesktopTest} alt="Select test gif" />
+                            <img className="mobileGif" src={InstrGif.MobileTest} alt="Select test gif" />
                     </BoxSection>
                     <br />
 
                     <BoxSection title={"3: Testing"}>
                             <h3>Start Game</h3>
+                            <img className="desktopGif" src={InstrGif.DesktopStart} alt="Start game gif" />
+                            <img className="mobileGif" src={InstrGif.MobileStart} alt="Start game gif" />
 
                             <h3>Guess Note</h3>
                             <p>CORRECT answer will light up in GREEN, WRONG answers will light up RED </p>
+                            <img className="desktopGif" src={InstrGif.DesktopGuess} alt="Correct is green, wrong is red gif" />
+                            <img className="mobileGif" src={InstrGif.MobileGuess} alt="Correct is green, wrong is red gif" />
 
                             <h3>Review, Replay or Restart</h3>
+                            <img className="desktopGif" src={InstrGif.DesktopEndGame} alt="End game gif" />
+                            <img className="mobileGif" src={InstrGif.MobileEndGame} alt="End ga,e gif" />
                     </BoxSection>
                     <br />
                     
